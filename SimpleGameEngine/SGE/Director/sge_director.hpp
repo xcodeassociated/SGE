8617480ce49f57f5e93c8c0572f4cf06802fdde4
Piotr Grudzień <piotr.grudzien@outlook.com>
2016-04-06 00:17:58 +0200

    Director class added.

---
+++ SimpleGameEngine/SGE/Director/sge_director.hpp
@@ -0,0 +1,21 @@
+#ifndef sge_director_h
+#define sge_director_h
+
+#include "../Scene/sge_scene.hpp"
+
+namespace SGE {
+    
+    class Director final{
+		
+	public:
+		static Director* getDirector();
+		Scene::ID addScene(Scene* s);
+		void deleteScene(Scene::ID s);
+		void showScene(Scene::ID s);
+		void swapScene(Scene::ID s);
+		void cleanUp();
+    };
+    
+}
+
+#endif /* sge_director_h */
