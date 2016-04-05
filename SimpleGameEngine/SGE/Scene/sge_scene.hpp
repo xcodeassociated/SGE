228fd0309d6123b853ce33c3f6f699423f6561c0
Piotr Grudzień <piotr.grudzien@outlook.com>
2016-04-06 00:15:29 +0200

    Scene class added.

---
+++ SimpleGameEngine/SGE/Scene/sge_scene.hpp
@@ -0,0 +1,18 @@
+#ifndef sge_scene_h
+#define sge_scene_h
+
+#include "../ID/sge_id.hpp"
+
+namespace SGE {
+    
+    class Scene{
+		
+	public:
+		class ID : public SGE::ID{
+			//todo
+		}
+    };
+    
+}
+
+#endif /* sge_scene_h */
