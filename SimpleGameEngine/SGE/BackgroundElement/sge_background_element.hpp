d0e6fb5970dae33a322d0255ea4951ac0e0d740b
Piotr Grudzień <piotr.grudzien@outlook.com>
2016-04-06 00:17:16 +0200

    BackgroundElement, WorldElement classes added.

---
+++ SimpleGameEngine/SGE/BackgroundElement/sge_background_element.hpp
@@ -0,0 +1,13 @@
+#ifndef sge_background_element_h
+#define sge_background_element_h
+
+#include "../Object/sge_object.hpp"
+
+namespace SGE {
+    
+    class BackgroundElement : public Object{
+    };
+    
+}
+
+#endif /* sge_background_element_h */
---
+++ SimpleGameEngine/SGE/BackgroundElement/sge_world_element.hpp
@@ -0,0 +1,13 @@
+#ifndef sge_world_element_h
+#define sge_world_element_h
+
+#include "sge_background_element.hpp"
+
+namespace SGE {
+    
+    class WorldElement : public BackgroundElement{
+    };
+    
+}
+
+#endif /* sge_world_element_h */
