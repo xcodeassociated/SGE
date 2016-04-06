27b43f73b07aa15d1055b5b94f970644a2a507f8
Piotr Grudzień <piotr.grudzien@outlook.com>
2016-04-06 14:48:54 +0200

    Utility to get path to executable added.

---
+++ SimpleGameEngine/SGE/Path/sge_path.hpp
@@ -0,0 +1,27 @@
+#ifndef sge_path_h
+#define sge_path_h
+
+#include "../include/sge_include.hpp"
+
+namespace SGE {
+	const std::string& getPath() {
+		static std::string base_path = []()->std::string{
+			char* path = SDL_GetBasePath();
+			std::string ret;
+			if(path == nullptr)
+			{
+				ret = std::string(path);
+				SDL_free(path);
+			}
+			else
+			{
+				ret = std::string("./");
+			}
+			return ret;
+		}();
+		return base_path;
+	}
+
+}
+
+#endif /* sge_path_h */
