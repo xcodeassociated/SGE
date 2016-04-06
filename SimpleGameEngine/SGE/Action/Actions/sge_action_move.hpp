73ae94ba1ba0b92056f8945f79f51e157b51fda3
Janusz Majchrzak <hamashy@gmail.com>
2016-04-06 14:46:16 +0200

    SubAction classes added.

---
+++ SimpleGameEngine/SGE/Action/Actions/sge_action_move.hpp
@@ -0,0 +1,13 @@
+//
+//  sge_action_move.hpp
+//  SimpleGameEngine
+//
+//  Created by Janusz Majchrzak on 06/04/16.
+//  Copyright © 2016 Hamashy. All rights reserved.
+//
+
+#ifndef sge_action_move_h
+#define sge_action_move_h
+
+
+#endif /* sge_action_move_h */
