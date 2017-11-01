fe3e909921137f848b6cd25afc5d20d390a6af0d
Piotr Grudzień <piotr.grudzien@outlook.com>
2017-11-01 18:25:15 +0100

    Added doxygen blocks to SceneLib.

--- SimpleGameEngine/Scene_lib/Scene/sge_scene.hpp
+++ SimpleGameEngine/Scene_lib/Scene/sge_scene.hpp
@@ -3,9 +3,9 @@
 
 #include <vector>
 #include <map>
-#include "sge_logic.hpp"
 #include "sge_level.hpp"
 #include "sge_scene_id.hpp"
+#include "sge_logic_bind.hpp"
 
 namespace SGE {
 	
@@ -14,32 +14,86 @@ namespace SGE {
 	class ObjectManager;
 
 
+	/**
+     * \brief 
+     */
     class Scene{
 	private:
-		using LogicVector = std::vector<Logic::Binder>;
+	    /**
+		 * \brief 
+		 */
+		using LogicVector = std::vector<LogicBind>;
+	    /**
+         * \brief 
+         */
         using ObjectVector= std::vector<ObjectID>;
         
+	    /**
+		 * \brief 
+		 */
 		Level level;
+	    /**
+		 * \brief 
+		 */
 		LogicVector Logics;
+	    /**
+         * \brief 
+         */
         ObjectVector* Objects = nullptr;
 
 	public:
+	    /**
+		 * \brief 
+		 */
 		bool TextureLoaded = false;
+	    /**
+		 * \brief 
+		 */
 		using ID = SceneID;
 
+	    /**
+	     * \brief 
+	     * \return 
+	     */
 	    LogicVector& getLogics();
 
+	    /**
+		 * \brief 
+		 */
 		virtual ~Scene() = default;
 
+	    /**
+	     * \brief 
+	     * \param path 
+	     * \param levelMask 
+	     */
 	    virtual void loadLevel(const char* path, std::map<char, std::string> levelMask) final;
 
+	    /**
+	     * \brief 
+	     * \return 
+	     */
 	    virtual Level& getLevel() final;
 
+	    /**
+	     * \brief 
+	     * \param obj 
+	     */
 	    virtual void BindObjects(ObjectVector* obj) final;
 
+	    /**
+	     * \brief 
+	     * \return 
+	     */
 	    virtual ObjectVector& getObjects() final;
 
+	    /**
+		 * \brief 
+		 */
 		virtual void finalize() = 0;
+	    /**
+         * \brief 
+         */
         virtual void onDraw() = 0;
 
     };
--- SimpleGameEngine/Scene_lib/Scene/sge_scene_id.hpp
+++ SimpleGameEngine/Scene_lib/Scene/sge_scene_id.hpp
@@ -11,13 +11,25 @@ namespace SGE {
 
     class Scene;
 
+	/**
+     * \brief 
+     * \todo Add missing access methods
+	 */
     class SceneID final : public ID {
         friend class Director;
         friend class ObjectManager;
 
+	    /**
+         * \brief 
+         */
         Scene* scene;
 
     public:
+	    /**
+         * \brief 
+         * \param id 
+         * \param s 
+         */
         SceneID(const long id, Scene* s);
     };
 
