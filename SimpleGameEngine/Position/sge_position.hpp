7c41d4ad5c0e9c8c9c5aff2f31b17cdcf6cf42a6
xcodeassociated <janusz.majchrzak@student.uj.edu.pl>
2019-02-25 00:12:20 +0100

    Add some additional API to the Position class

--- SimpleGameEngine/Position/sge_position.hpp
+++ SimpleGameEngine/Position/sge_position.hpp
@@ -1,6 +1,8 @@
 #ifndef sge_position_h
 #define sge_position_h
 
+#include <glm/glm.hpp>
+
 namespace SGE
 {
     struct Position
@@ -8,9 +10,33 @@ namespace SGE
         float x = 0;
         float y = 0;
 
-        Position(float _x, float _y) 
-            : x(_x), y(_y) {};
+        Position(float _x, float _y) : x(_x), y(_y) {};
         Position() = default;
+
+		void setX(const float x)
+		{
+			this->x = x;
+		}
+
+		void setY(const float y)
+		{
+			this->y = y;
+		}
+
+		float getX() const
+		{
+			return this->x;
+		}
+
+		float getY() const
+		{
+			return this->y;
+		}
+
+		glm::vec2 getPosition() const
+		{
+			return {this->x, this->y};
+		}
     };
 }
 #endif
